94b0f25d103047a6f8c81a5f22f1b2ec9542bd3e
Jeremy Newson <jknewson@usgs.gov>
2015-06-16 12:34:27 -0600

    added parameter group query and fixed parameter where clause

--- SStats/Assets/Scripts/BasinParametersTest.py
+++ SStats/Assets/Scripts/BasinParametersTest.py
@@ -6,7 +6,7 @@ import arcpy
 import shutil
 import json
 import logging
-import ArcHydroTools
+#import ArcHydroTools
 import xml.dom.minidom
 import decimal
 
@@ -24,7 +24,7 @@ class BasinParametersWrapper(object):
                 parser.add_argument("-directory", help="specifies the projects working directory", type=str, default = r"D:\gistemp\ClientData")
                 args = parser.parse_args()
                 
-                Results = {"Message": "INFO:D:\\gistemp\\ClientData\\IA20141201131238376000\\IA20141201131238376000.gdb\\Layers exists_INFO:workspace set: IA20141201131238376000_INFO:Stated calc params_AHMSG:Executing: StreamstatsGlobalParametersServer D:\\gistemp\\ClientData\\IA20141201131238376000\\IA20141201131238376000.gdb\\Layers\\GlobalWatershed D:\\gistemp\\ClientData\\IA20141201131238376000\\IA20141201131238376000.gdb\\Layers\\GlobalWatershedPoint DRNAREA;KSATSSUR;I24H10Y;CCM;TAU ANN;STREAM VAR;PRECIP;HYSEP;RSD D:\\gistemp\\ClientData\\IA20141201131238376000\\Temp\\parameterFile.xml D:\\gistemp\\ClientData\\IA20141201131238376000\\Temp\\parameterFile.htm D:\\gistemp\\ClientData\\IA20141201131238376000\\Temp\\StreamStatsIA.xml # IA20141201131238376000 Start Time: Mon Dec 01 14:14:22 2014 Performing global parameters computation... OID: 1\r Error when computing parameter FOS for the  watershed: \r E:\\ss data\\ia\\archydro\\07080105\\07080105.gdb\\Layers\\nhd fos\r D:\\gistemp\\ClientData\\IA20141201131238376000\\Temp\\IA20141201131238376000\\StreamsCountTmp\r ERROR 000210: Cannot create output C:\\Users\\JKNEWS~1\\AppData\\Local\\Temp\\4\\\\scratch.gdb\\DAStreamSJ\r Failed to execute (Spatial Join).\r Failed to execute (StreamsCount).. Succeeded at Mon Dec 01 14:14:26 2014 (Elapsed Time: 4.00 seconds)_INFO:finished calc params_INFO:parsing xml_INFO:finished_", "Parameters": [{"code": "DRNAREA", "value": "0.31"}, {"code": "STRMTOT", "value": "0.000"}, {"code": "FOS", "value": ""}, {"code": "KSATSSUR", "value": "4.61"}, {"code": "I24H10Y", "value": "4.44"}, {"code": "CCM", "value": "-99999.00"}, {"code": "TAU_ANN", "value": "20.39"}, {"code": "STREAM_VAR", "value": "0.66"}, {"code": "PRECIP", "value": "33.87"}, {"code": "HYSEP", "value": "53.95"}, {"code": "RSD", "value": ""}]}
+                Results = {"Message": "INFO:D:\\gistemp\\ClientData\\ME20150616080458803000\\ME20150616080458803000.gdb\\Layers exists_INFO:workspace set: ME20150616080458803000_INFO:Stated calc params_AHMSG:Executing: StreamstatsGlobalParametersServer D:\\gistemp\\ClientData\\ME20150616080458803000\\ME20150616080458803000.gdb\\Layers\\GlobalWatershedRaw D:\\gistemp\\ClientData\\ME20150616080458803000\\ME20150616080458803000.gdb\\Layers\\GlobalWatershedPoint PRECIP;PRDECFEB90;SANDGRAVAP;COASTDIST D:\\gistemp\\ClientData\\ME20150616080458803000\\Temp\\parameterFile.xml D:\\gistemp\\ClientData\\ME20150616080458803000\\Temp\\parameterFile.htm D:\\gistemp\\ClientData\\ME20150616080458803000\\Temp\\StreamStatsME.xml # ME20150616080458803000 Start Time: Tue Jun 16 11:52:02 2015 Performing global parameters computation... Succeeded at Tue Jun 16 11:52:07 2015 (Elapsed Time: 5.00 seconds)_INFO:finished calc params_INFO:parsing xml_INFO:finished_", "Parameters": [{"code": "CENTROIDX", "value": "497971.18"}, {"code": "CENTROIDY", "value": "5099424.03"}, {"code": "PRECIP", "value": "43.7"}, {"code": "PRDECFEB90", "value": "8.56"}, {"code": "SANDGRAVAP", "value": "0.00"}, {"code": "COASTDIST", "value": "146"}]}
 
             except:
                 Results = {"error":traceback.format_exc()}
